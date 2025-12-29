526b29961fbb5b702630c73dc506adcd3d00733c
Daniele Filice <59928344+danieleFFF@users.noreply.github.com>
2025-12-29 11:37:58 +0100

    project inizialization (backend)

---
+++ src/main/java/it/unical/xpoll/XPollApplication.java
@@ -0,0 +1,12 @@
+package it.unical.xpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XPollApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XPollApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unical/xpoll/XPollApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.xpoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XPollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
