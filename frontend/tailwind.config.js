9173151dc845282a4eeabb8e520f6b55496cd5a1
Antonio D'Ambrosio <ant05.dambrosio@gmail.com>
2025-12-30 17:52:55 +0100

    Created  empty pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#6297B1',
+                danger: '#852221',
+                'primary-container': '#C2DADF',
+                'on-primary-container': '#454659',
+                background: '#203452',
+                surface: '#344F78',
+                'on-primary': '#FFFFFF',
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            borderRadius: {
+                'card': '16px',
+                'btn': '50px',
+            },
+        },
     },
     plugins: [],
 }
